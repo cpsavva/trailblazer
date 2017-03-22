146877da37d4ff391ba476e3140b2c3cc1d655b0
cpsavva <csoriasavva@gmail.com>
2017-03-22 14:14:51 -0400

    playing with logic

--- routes/trail-routes.js
+++ routes/trail-routes.js
@@ -1,5 +1,6 @@
 var models = require('../models');
 var unirest = require('unirest');
+var axios = require('axios');
 
 
 
@@ -8,24 +9,42 @@ module.exports = function(app) {
     app.get("/api/:latitude/:longitude", function(req, res) {
         var latitude = req.params.latitude;
         var longitude = req.params.longitude;
-        unirest.get("https://trailapi-trailapi.p.mashape.com/?lat=" + latitude + "&limit=25&lon=" + longitude + "&q[activities_activity_type_name_eq]=hiking&radius=50")
-            .header("X-Mashape-Key", "1EUZc9Yh0Dmsh3NULqLjzLCBf7rsp1iedcgjsnE14nUri24ZVA")
-            .header("Accept", "text/plain")
-            .end(function(result) {
-                var trailList = result.body.places;                 
-                var trails=[]; 
-                trailList.forEach(function(arrayItem){
-                	var trail={
-                		trail_name:arrayItem.activities[0].name,
-                		description:arrayItem.activities[0].description,
-                		distance:arrayItem.activities[0].length,
-                		url:arrayItem.activities[0].url
-                	}
-                	trails.push(trail);
-                })       
+        axios.get("https://trailapi-trailapi.p.mashape.com/?lat=" + latitude + "&limit=25&lon=" + longitude + "&q[activities_activity_type_name_eq]=hiking&radius=50",{
+            
+            headers: {
+                "X-Mashape-Key": "1EUZc9Yh0Dmsh3NULqLjzLCBf7rsp1iedcgjsnE14nUri24ZVA",
+                "Accept": "text/plain"
+            }
+        }).then(function(result){
+            console.log(result.data.places);
+            var places = result.data.places
+            for(i=0; i< places.length; i++){
+                models.Trail.create{
+                    
+                }
+            }
+
+            res.end();
+        });
+        res.redirect('/trails')
+        // unirest.get("https://trailapi-trailapi.p.mashape.com/?lat=" + latitude + "&limit=25&lon=" + longitude + "&q[activities_activity_type_name_eq]=hiking&radius=50")
+        //     .header("X-Mashape-Key", "1EUZc9Yh0Dmsh3NULqLjzLCBf7rsp1iedcgjsnE14nUri24ZVA")
+        //     .header("Accept", "text/plain")
+        //     .end(function(result) {
+        //         var trailList = result.body.places;                 
+        //         var trails=[]; 
+        //         trailList.forEach(function(arrayItem){
+        //         	var trail={
+        //         		trail_name:arrayItem.activities[0].name,
+        //         		description:arrayItem.activities[0].description,
+        //         		distance:arrayItem.activities[0].length,
+        //         		url:arrayItem.activities[0].url
+        //         	}
+        //         	trails.push(trail);
+        //         })       
                	
-                res.render("trails", { results: trails });
-            });
+        //         res.render("trails", { results: trails });
+        //     });
 
     });
 }
